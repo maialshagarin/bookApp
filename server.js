787ecdd1635a7a6dbd9875e05e88a8529110df21
maialshagarin <mai.yusuf28@yahoo.com>
2020-09-19 21:11:05 +0300

    bulding the first part of getting data from the api and render it use ejs

--- server.js
+++ server.js
@@ -3,13 +3,41 @@
 const express = require('express') ;
 require('dotenv').config();
 const app = express();
-const port = process.env.port || 3000 ;
+const port = process.env.PORT || 3000 ;
+const superagent = require('superagent')
+
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('pages/index')
-  });
+app.use(express.static('./public'));
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// To Read the body of the POST HTTP requsets
+app.use(express.urlencoded());
+
+
+app.get('/', getSearchHanlde);
+
+  function getSearchHanlde(req, res) {
+    res.render('pages/index');
+  }
+
+  app.post('/searches',  postSearchfunction);
+  function postSearchfunction(req , res ){
+    let data = req.body ; 
+    console.log(data)
+    let queryName = Object.keys(data)[1];
+    let name = data.BookTitle ;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${name}+in${queryName}`;
+    superagent.get(url)
+              .then(result =>{
+                console.log("result" , result)
+              })
+
+  }
+
 
 
 app.listen(port , ()=> console.log('baby step '))
